174eb3b7fba796647f161b565840d9662ac543c1
ALeksandrov <a3alex89@gmail.com>
2022-11-29 18:47:57 +0200

    added not found page component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    WelcomeComponent,
+    NotFoundComponent
+  ],
   imports: [
     CommonModule
   ]
