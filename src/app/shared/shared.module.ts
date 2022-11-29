e0dbd6c10f9f4c43b03960a28820c631d3f4a502
ALeksandrov <a3alex89@gmail.com>
2022-11-29 20:10:33 +0200

    added welcome msg to themes component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
     WelcomeComponent,
-    NotFoundComponent
+    NotFoundComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports:[
+    WelcomeComponent,
   ]
 })
 export class SharedModule { }
