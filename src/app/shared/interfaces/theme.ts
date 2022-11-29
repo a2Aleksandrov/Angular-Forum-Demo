7b30095fb9703090ef592af777063ef6ff8efed4
ALeksandrov <a3alex89@gmail.com>
2022-11-29 20:10:55 +0200

    fixed theme interface

--- src/app/shared/interfaces/theme.ts
+++ src/app/shared/interfaces/theme.ts
@@ -2,6 +2,21 @@ export interface ITheme {
     _id: string;
     subscribers: string[];
     themeName: string;
+    posts: {
+        _id: string;
+        text: string;
+        likes: string[];
+        themeId: string;
+        userId: {
+            _id: string;
+            username: string;
+            email: string;
+            tel: string;
+            themes: string[];
+        };
+        created_at: string;
+        updatedAt: string;
+    }[];
     userId: {
         _id: string;
         username: string;
