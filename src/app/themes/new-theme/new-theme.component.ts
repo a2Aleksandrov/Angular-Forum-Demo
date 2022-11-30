9c50266e817e7bb38c1d4a822cfb1f66670a1758
ALeksandrov <a3alex89@gmail.com>
2022-11-30 15:16:58 +0200

    added new theme form validation

--- src/app/themes/new-theme/new-theme.component.ts
+++ src/app/themes/new-theme/new-theme.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-theme',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewThemeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  addTheme() {
+
+  }
+
+  cancelTheme() {
+    this.router.navigate(['/themes']);
+  }
 }
