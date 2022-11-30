7716f8be0773e2b3f76e4a80d42ea0c3bf546d97
ALeksandrov <a3alex89@gmail.com>
2022-11-30 19:00:28 +0200

    updated user interface

--- src/app/shared/interfaces/user.ts
+++ src/app/shared/interfaces/user.ts
@@ -1,3 +1,12 @@
-export interface IUser {
-    
+export interface ICreateUser {
+    username: string;
+    email: string;
+    tel: string;
+    password: string;
+}
+
+export interface ILogInUser {
+     email: string;
+     username: string;
+     password: string;
 }
